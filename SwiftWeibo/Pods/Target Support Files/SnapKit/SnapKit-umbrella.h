2b2e4d73215c9a22c8c2e5df5af4b47457c1ab3b
coderwjq <coderwjq@163.com>
2016-11-02 00:58:19 +0800

    添加SnapKit依赖
    更新cocoapods到1.2版本,然后添加SnapKit库,已支持Swift3.

--- SwiftWeibo/Pods/Target Support Files/SnapKit/SnapKit-umbrella.h
+++ SwiftWeibo/Pods/Target Support Files/SnapKit/SnapKit-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_SwiftWeiboVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SwiftWeiboVersionString[];
+FOUNDATION_EXPORT double SnapKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char SnapKitVersionString[];
 
